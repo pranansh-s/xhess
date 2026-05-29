47617601651b5429d137f72fb136e1190c1fafa4
pranansh-s <prananshsingh@gmail.com>
2026-05-29 06:16:48 +0530

    Redis production moved to Upstash

--- web/src/hooks/useForm.ts
+++ web/src/hooks/useForm.ts
@@ -1,14 +1,13 @@
 import { useState } from 'react';
 
 import { IInputValue } from '@/types';
-import { ZodError } from 'zod/v4';
+import { ZodError } from 'zod';
 
-//update hook to be more precise and friendly with schema
 export const useForm = () => {
   const [formState, setFormState] = useState<{ [key: string]: IInputValue }>({});
   const hasErrors = Object.values(formState).some(input => input?.error);
 
-  const handleInputChange = (schema: any) => (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (schema: { parse: (val: unknown) => unknown }) => (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     try {
       schema.parse(value);
